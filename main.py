683f76d39ba4d0db90cec1fcb3129f1211fb439c
ziro <petrop@purdue.edu>
2021-12-30 17:23:37 -0600

    working, but not conveniently image opening a new window forces user out of text input field

--- main.py
+++ main.py
@@ -5,44 +5,75 @@ import os
 from os import listdir
 from os.path import isfile, join
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-def openimage():
-    filepath = os.getcwd()
-    print(filepath)
-    filepath+= "\\truth.PNG"
+def openimage(dir,suffix):
+    path = dir + suffix
 
-    img = Image.open(r"C:\Users\padop\PycharmProjects\ImageSorter\pizza.jpg")
-
+    img = Image.open(path)
     img.show()
+    print("New filename for:",suffix)
+    inputval = input()
+
+    img.close()
+    return inputval
 
 def getdir():
     print("Would you like to use the current directory or a custom one? (CURR/CUST)")
     inputval = input()
-    if ((inputval == 'CURR') | (inputval =='current') | (inputval =='CURRENT') | (inputval =='Current') | (inputval =='curr')):
+    inputval = inputval.lower()
+    if ((inputval == 'curr') | (inputval =='current')):
         print("Using current directory...")
         inputval = os.getcwd()
+        inputval += ('\\')
     else:
         inputval = input("Custom directory:")
         #check if they inputted a \ at the end
         if (inputval[len(inputval)-1] != '\\'):
             inputval+=('\\')
-        print("Using custom directory:",inputval)
+        #print("Using custom directory:",inputval)
 
+    inputval = inputval.replace('\\','/')
+    print("Using directory:", inputval)
     return inputval
 
 def getimages(filelist):
+    finlist = []
+    for i in filelist:
+        lngth = len(i)
+        extension = (i[(lngth-4):(lngth)])
+        extension = extension.lower()
+        if ((extension == '.png') | (extension == '.jpg')): #| if I add support for jpeg, I will need to change the renaming part of main (extension == 'jpeg')
+            finlist.append(i)
+    return finlist
 
 if __name__ == '__main__':
+    #initializations
+    inputval = ""
 
+    #RENAME MODE
     dir = getdir()
 
-    onlyfiles = [f for f in listdir(dir) if isfile(join(dir, f))]
+    onlyfiles = [f for f in listdir(dir) if isfile(join(dir, f))] #thanks to pycruft from https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory
 
-    print(onlyfiles)
-    inputval = ""
-    while (inputval != 'exit'):
-        inputval = input("New filename:")
+    imgs = getimages(onlyfiles)
+
+    print("To keep the filename, simply press space")
+    print("To stop filenaming, enter 'exit'")
+
+    for suffix in imgs:
+        response = openimage(dir,suffix)
+
+        #check if it exit
+        if response == 'exit':
+            break
+        #check rename
+        if response != ' ': #sets it up so that if it is not the space needed to reset, rename the file. OW, proceed to next file
+            path = dir + suffix
+            extension = (suffix[(len(suffix) - 4):(len(suffix))])
+
+            os.rename(path,dir+response+extension)
+            #print(dir+response+extension)
+
+
+
     print("done")
